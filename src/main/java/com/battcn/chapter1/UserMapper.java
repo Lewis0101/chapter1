1e2dbc8c99dd970a68b6e7688f95b49b34b4e3fa
LiuYang <1005859278@qq.com>
2019-03-21 18:05:11 +0800

    Mybatis框架

---
+++ src/main/java/com/battcn/chapter1/UserMapper.java
@@ -0,0 +1,40 @@
+package com.battcn.chapter1;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author : 00222 [liu.yang@unisinsight.com]
+ * @description:
+ * @date : 2019/3/21 14:25
+ * @since: 1.0
+ */
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 获取视频信息
+     * 通过注解实现
+     * @return 查询结果
+     */
+    @Select("SELECT * FROM video_info")
+    List<Video> getVideoInfo();
+
+    /**
+     * 获取视频信息
+     * 在xml里面实现
+     * @return 查询结果
+     */
+    List<Video> getVideoInfo1();
+
+    /**
+     * 保存用户信息
+     *
+     * @param user 用户信息
+     * @return 成功 1 失败 0
+     */
+    int insert(Video user);
+
+}
---
+++ src/main/java/com/battcn/chapter1/Video.java
@@ -0,0 +1,61 @@
+package com.battcn.chapter1;
+
+import java.io.Serializable;
+
+/**
+ * @author : 00222 [liu.yang@unisinsight.com]
+ * @description:
+ * @date : 2019/3/20 14:01
+ * @since: 1.0
+ */
+
+public class Video implements Serializable {
+
+    private static final long serialVersionUID = 8655851615465363473L;
+
+    private String videoId;
+    private String videoName;
+    private String fileFormat;
+    private String contentDescription;
+    private String deviceId;
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getVideoName() {
+        return videoName;
+    }
+
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+    public String getFileFormat() {
+        return fileFormat;
+    }
+
+    public void setFileFormat(String fileFormat) {
+        this.fileFormat = fileFormat;
+    }
+
+    public String getContentDescription() {
+        return contentDescription;
+    }
+
+    public void setContentDescription(String contentDescription) {
+        this.contentDescription = contentDescription;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+}
